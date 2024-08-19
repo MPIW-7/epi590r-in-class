0a2907ae67cf151e257dc1a85eea811ea418831f
MPIW-7 <maria.paula.ibarcena.woll@emory.edu>
2024-08-19 11:08:48 -0400

    Error

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -1,9 +1,11 @@
+
+
 nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+setwd("C:\Users\maria\OneDrive\Desktop\EPI 590R R Bootcamp\EPI590R\R")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
